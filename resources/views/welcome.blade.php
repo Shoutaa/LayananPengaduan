c18f3083656207b82539e62c170c0016875f9558
Shoutaa <alifhamizan66@gmail.com>
2025-01-17 22:56:55 +0700

    update welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,10 +53,6 @@
 
   </section><!-- End Hero -->
 
-  <main id="main">
-
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
   @include('includes.landing.javascript')
 
 </body>
